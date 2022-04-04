5496e6b97f0e136889cd43746daedb4212f953b7
Matheus Mariano Poda <matheus.poda@gmail.com>
2022-04-04 09:54:57 -0300

    Adicionado script de criação de banco de dados

--- src/main/java/io/github/matskira/VendasApplication.java
+++ src/main/java/io/github/matskira/VendasApplication.java
@@ -1,13 +1,10 @@
 package io.github.matskira;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class VendasApplication {
 	
 
