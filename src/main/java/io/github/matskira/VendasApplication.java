32a535690624891dccf0c41f22c2894e40130b5e
Matheus Mariano Poda <matheus.poda@gmail.com>
2022-04-05 14:05:26 -0300

    Colocado pesquisa de produto por cliente

--- src/main/java/io/github/matskira/VendasApplication.java
+++ src/main/java/io/github/matskira/VendasApplication.java
@@ -1,6 +1,9 @@
 package io.github.matskira;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -9,46 +12,34 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 import io.github.matskira.domain.entity.Cliente;
+import io.github.matskira.domain.entity.Pedido;
 import io.github.matskira.domain.repository.ClienteRepository;
+import io.github.matskira.domain.repository.PedidoRepository;
 
 @SpringBootApplication
 public class VendasApplication {
 	
 	@Bean
-	public CommandLineRunner init(@Autowired ClienteRepository clienteRep) {
+	public CommandLineRunner init(@Autowired ClienteRepository clienteRep,
+			@Autowired PedidoRepository pedidoRep) {
 		return args -> {
 			System.out.println("SALVANDO CLIENTES");
-			clienteRep.save(new Cliente("Matheus Poda"));
-			clienteRep.save(new Cliente("Amanda Costa"));
-			clienteRep.save(new Cliente("Alessandra Matos"));
+			Cliente fulano = new Cliente("Matheus Poda");
+			clienteRep.save(fulano);
+
+			Pedido p = new Pedido();
+			p.setCliente(fulano);
+			p.setDataPedido(LocalDate.now());
+			p.setTotal(BigDecimal.valueOf(100));
+			
+			pedidoRep.save(p);
+			
+			pedidoRep.findByCliente(fulano).forEach(System.out::println);
+			
 			
 			List<Cliente> todosClientes = clienteRep.findAll();
 			todosClientes.forEach(System.out::println);
-			
-			System.out.println("ATUALIZANDO CLIENTES");
-			todosClientes.forEach(c->{
-				c.setNome(c.getNome()+" ATUALIZADO");
-				clienteRep.save(c);
-			});
-			
-			System.out.println("PROCURANDO CLIENTES");
-			//No caso ao colocar o Contains, ele busca na base qualquer nome que tenha os caracteres e não o específico
-			clienteRep.findByNomeContains("Amanda").forEach(System.out::println);
-			
-			boolean existe = clienteRep.existsByNome("Amanda Costa ATUALIZADO");
-			System.out.println("Existe alguém com o nome Amanda? "+existe);
-			
-			System.out.println("DELETANDO CLIENTES");
-			clienteRep.findAll().forEach(c -> {
-				clienteRep.delete(c);
-			});
-			
-			todosClientes = clienteRep.findAll();
-			if (todosClientes.isEmpty()) {
-				System.out.println("Nenhum cliente encontrado");
-			}else {
-				todosClientes.forEach(System.out::println );
-			}
+		
 			
 			
 			
